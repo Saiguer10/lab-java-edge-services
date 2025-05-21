7b9f8248ad676d9d1f291d05dfbde3bfdbd3315c
Saiguer10 <eljosemsp@gmail.com>
2025-05-21 16:51:37 +0200

    "Commit con solucion de lab corriendo"

--- Gateway-api/src/main/java/com/example/demo/config/GatewayConfig.java
+++ Gateway-api/src/main/java/com/example/demo/config/GatewayConfig.java
@@ -9,26 +9,19 @@ import org.springframework.context.annotation.Configuration;
 public class GatewayConfig {
 
     @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
         return builder.routes()
-                .route("student_route", r -> r
-                        .path("/api/students/**")
-                        .filters(f -> f.rewritePath("/api/students/(?<segment>.*)", "/students/${segment}"))
-                        .uri("lb://student-info-service")
-                )
-                .route("grades_route", r -> r
-                        .path("/api/grades/**", "/api/courses/**")
-                        .filters(f -> f
-                                .rewritePath("/api/grades/(?<segment>.*)", "/grades/${segment}")
-                                .rewritePath("/api/courses/(?<segment>.*)", "/courses/${segment}")
-                        )
-                        .uri("lb://grades")
-                )
-                .route("catalog_route", r -> r
-                        .path("/api/catalog/**")
-                        .filters(f -> f.rewritePath("/api/catalog/(?<segment>.*)", "/catalog/${segment}"))
-                        .uri("lb://student-catalog-service")
-                )
+                .route("student",r->r.path("/api/student/**")
+                        .uri("lb://student-info-service"))
+
+                .route("course",r->r.path("/api/course/**")
+                        .uri("lb://grade-data-service"))
+
+                .route("grade",r->r.path("/api/grade/**")
+                        .uri("lb://grade-data-service"))
+                .route("catalog",r->r.path("/api/catalog/**")
+                        .uri("lb://student-catalog-service"))
+
                 .build();
     }
 }
